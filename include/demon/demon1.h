a727f81807f08e1b4bbfe5f15453641fbc788cd1
PSielchan <147639332+PSielchan@users.noreply.github.com>
2025-12-13 16:10:45 +0100

    demon1

---
+++ include/demon/demon1.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <archimedes/Scene.h>
+
+using namespace arch;
+
+class demon1 {
+public:
+ //    void show();
+	// void hide();
+	static void setup(Scene& scene);
+};
+
+struct Demon1Flag {
+	static constexpr bool flagComponent = true;
+};
