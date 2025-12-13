1fb215670cbcd3b28132f695667d8cf8cb8b3409
PSielchan <147639332+PSielchan@users.noreply.github.com>
2025-12-13 16:34:49 +0100

    demons positions

---
+++ include/demon/demon3.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <archimedes/Scene.h>
+
+using namespace arch;
+
+class demon3 {
+public:
+    //    void show();
+    // void hide();
+    static void setup(Scene& scene);
+};
+
+struct Demon3Flag {
+    static constexpr bool flagComponent = true;
+};
