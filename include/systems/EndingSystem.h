7f25d527a7634ae647047368b5c10d8c0de9506c
MagrosThornrime <magros.thornrime@gmail.com>
2025-12-14 13:46:29 +0100

    Rename bad end func

--- include/systems/EndingSystem.h
+++ include/systems/EndingSystem.h
@@ -5,5 +5,5 @@
 using namespace arch;
 
 struct EndingSystem {
-	static void end(Scene& scene, std::string_view texturePath, std::string_view endingTheme);
+	static void badEnd(Scene& scene, std::string_view texturePath, std::string_view endingTheme);
 };
