f9425dab516272e5ca0cb1c875db4c26c403e3d2
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2025-12-14 14:17:09 +0100

    textures + skip curscene

--- src/demon/demon1.cpp
+++ src/demon/demon1.cpp
@@ -4,42 +4,90 @@
 #include <Config.h>
 #include <Defaults.h>
 
+struct Demon1BodyFlag {
+	static constexpr bool flagComponent = true;
+};
+
 void demon1::setup(Scene& scene) {
-    auto head = scene.newEntity();
-    auto headTexture = makeTexture("textures/Asset_szkice/Demon1_glowa_asset.png");
-    auto&& renderer = *gfx::Renderer::current();
-    head.addComponent<Demon1Flag>();
-    auto&& transform = head.addComponent(
-        scene::components::TransformComponent{
-            .position = {windowWidth /4, windowHeight*0.3, 1},
-            .rotation = {0, 0, 0, 1},
-            .scale = {headTexture->getWidth()*0.8, headTexture->getHeight()*0.8, 0}
-        }
-    );
-    auto mesh = makeMesh(defaultCenterVertices(), defaultIndices());
-    auto pipeline = renderer.getPipelineManager()->create(
-        gfx::pipeline::Pipeline::Desc{
-            .vertexShaderPath = "shaders/vertex_default.glsl",
-            .fragmentShaderPath = "shaders/fragment_default.glsl",
-            .textures = {std::move(headTexture)},
-            .buffers = {defaultUniformBuffer()},
-        }
-        );
-    auto&& meshComp = head.addComponent(
-        scene::components::MeshComponent{
-            .mesh = mesh,
-            .pipeline = pipeline
-        }
-    );
+	{
+		auto head = scene.newEntity();
+		auto headTexture = makeTexture("textures/demon1.png");
+		auto&& renderer = *gfx::Renderer::current();
+		head.addComponent<Demon1Flag>();
+		auto&& transform = head.addComponent(
+			scene::components::TransformComponent{
+				.position = {windowWidth / 2, windowHeight / 2, 1},
+				.rotation = {0, 0, 0, 1},
+				.scale = float3{headTexture->getWidth(), headTexture->getHeight(), 0} *0.9f
+			}
+		);
+		auto mesh = makeMesh(defaultCenterVertices(), defaultIndices());
+		auto pipeline = renderer.getPipelineManager()->create(
+			gfx::pipeline::Pipeline::Desc{
+				.vertexShaderPath = "shaders/vertex_default.glsl",
+				.fragmentShaderPath = "shaders/fragment_default.glsl",
+				.textures = {std::move(headTexture)},
+				.buffers = {defaultUniformBuffer()},
+			}
+			);
+		auto&& meshComp = head.addComponent(
+			scene::components::MeshComponent{
+				.mesh = mesh,
+				.pipeline = pipeline
+			}
+		);
+	}
+	{
+		auto head = scene.newEntity();
+		auto headTexture = makeTexture("textures/demon1_2.png");
+		auto&& renderer = *gfx::Renderer::current();
+		head.addComponent<Demon1BodyFlag>();
+		auto&& transform = head.addComponent(
+			scene::components::TransformComponent{
+				.position = {windowWidth / 2, windowHeight / 2, 1},
+				.rotation = {0, 0, 0, 1},
+				.scale = float3{headTexture->getWidth(), headTexture->getHeight(), 0} *0.9f
+			}
+		);
+		auto mesh = makeMesh(defaultCenterVertices(), defaultIndices());
+		auto pipeline = renderer.getPipelineManager()->create(
+			gfx::pipeline::Pipeline::Desc{
+				.vertexShaderPath = "shaders/vertex_default.glsl",
+				.fragmentShaderPath = "shaders/fragment_default.glsl",
+				.textures = {std::move(headTexture)},
+				.buffers = {defaultUniformBuffer()},
+			}
+			);
+		auto&& meshComp = head.addComponent(
+			scene::components::MeshComponent{
+				.mesh = mesh,
+				.pipeline = pipeline
+			}
+		);
+	}
 }
 void demon1::show(Scene& scene) {
-    auto head = scene.entitiesWith<Demon1Flag>().front();
-    auto && transform = head.getComponent<scene::components::TransformComponent>();
-    transform.position.z=-0.4;
+	{
+		auto head = scene.entitiesWith<Demon1Flag>().front();
+		auto&& transform = head.getComponent<scene::components::TransformComponent>();
+		transform.position.z = -0.4;
+	}
+	{
+		auto body = scene.entitiesWith<Demon1BodyFlag>().front();
+		auto&& transform = body.getComponent<scene::components::TransformComponent>();
+		transform.position.z = -0.505;
+	}
 
 }
 void demon1::hide(Scene& scene) {
-    auto head = scene.entitiesWith<Demon1Flag>().front();
-    auto && transform = head.getComponent<scene::components::TransformComponent>();
-    transform.position.z=1;
+	{
+		auto head = scene.entitiesWith<Demon1Flag>().front();
+		auto&& transform = head.getComponent<scene::components::TransformComponent>();
+		transform.position.z = 1;
+	}
+	{
+		auto head = scene.entitiesWith<Demon1BodyFlag>().front();
+		auto&& transform = head.getComponent<scene::components::TransformComponent>();
+		transform.position.z = 1;
+	}
 }
--- src/demon/demon2.cpp
+++ src/demon/demon2.cpp
@@ -5,41 +5,41 @@
 #include <Defaults.h>
 
 void demon2::setup(Scene& scene) {
-    auto head = scene.newEntity();
-    auto headTexture = makeTexture("textures/demon_nr2_dyzio.png");
-    auto&& renderer = *gfx::Renderer::current();
-    head.addComponent<Demon2Flag>();
-    auto&& transform = head.addComponent(
-        scene::components::TransformComponent{
-            .position = {windowWidth *0.7, windowHeight*0.43, 1},
-            .rotation = {0, 0, 0, 1},
-            .scale = {headTexture->getWidth()*0.8, headTexture->getHeight()*0.8, 0}
-        }
-    );
-    auto mesh = makeMesh(defaultCenterVertices(), defaultIndices());
-    auto pipeline = renderer.getPipelineManager()->create(
-        gfx::pipeline::Pipeline::Desc{
-            .vertexShaderPath = "shaders/vertex_default.glsl",
-            .fragmentShaderPath = "shaders/fragment_default.glsl",
-            .textures = {std::move(headTexture)},
-            .buffers = {defaultUniformBuffer()},
-        }
-        );
-    auto&& meshComp = head.addComponent(
-        scene::components::MeshComponent{
-            .mesh = mesh,
-            .pipeline = pipeline
-        }
-    );
+	auto head = scene.newEntity();
+	auto headTexture = makeTexture("textures/demon2.png");
+	auto&& renderer = *gfx::Renderer::current();
+	head.addComponent<Demon2Flag>();
+	auto&& transform = head.addComponent(
+		scene::components::TransformComponent{
+			.position = {windowWidth * 0.7, windowHeight * 0.43, 1},
+			.rotation = {0, 0, 0, 1},
+			.scale = float3{headTexture->getWidth(), headTexture->getHeight(), 0} *0.9f
+		}
+	);
+	auto mesh = makeMesh(defaultCenterVertices(), defaultIndices());
+	auto pipeline = renderer.getPipelineManager()->create(
+		gfx::pipeline::Pipeline::Desc{
+			.vertexShaderPath = "shaders/vertex_default.glsl",
+			.fragmentShaderPath = "shaders/fragment_default.glsl",
+			.textures = {std::move(headTexture)},
+			.buffers = {defaultUniformBuffer()},
+		}
+		);
+	auto&& meshComp = head.addComponent(
+		scene::components::MeshComponent{
+			.mesh = mesh,
+			.pipeline = pipeline
+		}
+	);
 }
 void demon2::show(Scene& scene) {
-    auto head = scene.entitiesWith<Demon2Flag>().front();
-    auto && transform = head.getComponent<scene::components::TransformComponent>();
-    transform.position.z=-0.7;
+	auto head = scene.entitiesWith<Demon2Flag>().front();
+	auto&& transform = head.getComponent<scene::components::TransformComponent>();
+	transform.position.z = -0.499;
 
 }
 void demon2::hide(Scene& scene) {
-    auto head = scene.entitiesWith<Demon2Flag>().front();
-    auto && transform = head.getComponent<scene::components::TransformComponent>();
-    transform.position.z=1;
+	auto head = scene.entitiesWith<Demon2Flag>().front();
+	auto&& transform = head.getComponent<scene::components::TransformComponent>();
+	transform.position.z = 1;
 }
